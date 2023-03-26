cac8ad47cc162eb0d075fb29e2448d9e59ffbf42
Apricitine <apricitinecas@gmail.com>
2023-03-26 16:36:26 -0700

    Try to get Storyblok Working

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="moasdf">
-    Hello
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [['@storyblok/nuxt', { accessToken: "mIW6wfyaTflxX9cAmBWBgAtt" }]],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
